8a80027ad5cb7b743f21ad08fe47ebcf32104234
zeynepmerveyanar <zmervey18@gmail.com>
2021-04-20 14:18:04 +0100

    Added Workouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from "react"
 import "./App.css"
+import {useEffect, useState} from "react"
+
 
 import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom'
 
 // Importing High Level Components
-import Login from './components/highLevel/Login'
-import Homepage from './components/highLevel/Homepage'
-import Register from "./components/highLevel/Register"
-import About from "./components/highLevel/About"
+import Login from './components/pages/Login'
+import Homepage from './components/pages/Homepage'
+import Register from "./components/pages/Register"
+import About from "./components/pages/About"
+import Workout from "./components/pages/Workout"
 
 // Importing Low Level Components
 import Footer from './components/lowLevel/Footer'
 import Header from "./components/lowLevel/Header"
+import Button from "./components/lowLevel/Button"
 
 function App() {
+  
+
+
   return (
     <Router>
       <Header/>
@@ -22,7 +29,9 @@ function App() {
       <Route exact path="/login" component={Login}/> 
       <Route exact path="/register" component={Register}/> 
       <Route exact path="/About" component={About}/> 
+      <Route exact path="/workout" component={Workout}/>
 
+      <Link to="/workout"><Button buttonName={'Workout'}/></Link>
       <Footer/>
     </Router>
    
