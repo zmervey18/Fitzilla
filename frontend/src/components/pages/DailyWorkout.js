b37005d774350955de574a623e5f50ca104895e9
zeynepmerveyanar <zmervey18@gmail.com>
2021-04-22 18:28:49 +0100

    Added image links

--- frontend/src/components/pages/DailyWorkout.js
+++ frontend/src/components/pages/DailyWorkout.js
@@ -9,12 +9,12 @@ const DailyWorkout = ({workout}) => {
 
     const imagesDaily = {
         "Triceps":"https://tinyurl.com/2z7hruhd",
-        'Biceps':'',
-        'Shoulders':'',
-        "Back":'',
-        "Chest":'',
-        "Legs":'',
-        "Arms":'',
+        'Biceps':'https://tinyurl.com/3cr4e9mt',
+        'Shoulders':'https://tinyurl.com/9k5z5d97',
+        "Back":'https://tinyurl.com/2sus522j',
+        "Chest":'https://tinyurl.com/3bvvvj6s',
+        "Legs":'https://tinyurl.com/yyh57k59',
+        "Arms":'https://tinyurl.com/xu9ap9e2',
     };
 
     return (
